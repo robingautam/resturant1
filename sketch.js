041a9703dbaec545cba4f1909d5824a0f2558e12
robingautam <31219114+robingautam@users.noreply.github.com>
2018-05-12 18:49:59 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,50 @@
 $(document).ready(function() {
     $('#rob').slideUp(function() {
-        $(this).show('5000');
+        $(this).show('3000');
     });
  $('#menu1').ready(function() {
     $('#menu1').slideUp(function() {
-        $(this).show(5000);
+        $(this).show(3000);
     });
 });
 $('#menu2').ready(function() {
     $('#menu2').slideUp(function() {
-        $(this).show(5000);
+        $(this).show(3000);
     });
 });
     $('#m1').ready(function() {
     $('#m1').slideUp(function() {
-        $(this).show(15000);
+        $(this).show(8000);
     });
 });
 $('#m2').ready(function() {
     $('#m2').slideUp(function() {
-        $(this).show(15000);
+        $(this).show(8000);
     });
 });
     $('#m3').ready(function() {
     $('#m3').slideUp(function() {
-        $(this).show(15000);
+        $(this).show(8000);
     });
 });
     $('#m4').ready(function() {
     $('#m4').slideUp(function() {
-        $(this).show(15000);
+        $(this).show(8000);
     });
 });
     $('#table').ready(function() {
         $('#table').slideUp(function() {
-            $(this).show(30000);
+            $(this).show(10000);
         });
     });
     $('#rob1').ready(function() {
         $('#rob1').slideUp(function() {
-            $(this).show(25000);
+            $(this).show(10000);
         });
     });
     $('#rob2').ready(function() {
         $('#rob2').slideUp(function() {
-            $(this).show(25000);
+            $(this).show(10000);
         });
     });
     
